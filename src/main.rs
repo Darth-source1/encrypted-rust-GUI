760568d020188ef48bcba4c784a55f7bd6a860b7
Shrihan-P <>
2021-07-02 14:06:26 -0400

    list and return fuction

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 fn main() {
-    let welcome = "welcome";
-    println!("el valor de {}",welcome);
+    let welcome: &str = "welcome";
+    let mil = welcome;
+    println!("el valor de {}",mil);
+    let list: [i32; 3] = [1, 2, 3];
+    println!("el segundo numero es {}",list[1]);
+    println!("la cantidad de numeros son {}",list.len());
+
+    let f: fn(i32) -> i32 = plus_one;
 }
+
+fn plus_one(i: i32) -> i32 {
+    i + 1
+}
+
