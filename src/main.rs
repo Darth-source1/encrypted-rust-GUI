096b621febf66291eb994b885d089d60180a011a
Shrihan-P <shrihan.p1@gmail.com>
2021-07-03 02:28:06 -0400

    basic structure in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-fn main() {
-    let welcome: &str = "welcome";
-    let mil = welcome;
-    println!("el valor de {}",mil);
-    let list: [i32; 3] = [1, 2, 3];
-    println!("el segundo numero es {}",list[1]);
-    println!("la cantidad de numeros son {}",list.len());
+/// #proof of concept
 
-    let f: fn(i32) -> i32 = plus_one;
+fn main() {
+welcome();
 }
 
-fn plus_one(i: i32) -> i32 {
-    i + 1
+fn welcome() {
+    println!("welcome select a option \n");
+    println!("1- encrypt a string");
+    println!("2- encrypt a file");
+    println!("3- decrypt");
 }
 
